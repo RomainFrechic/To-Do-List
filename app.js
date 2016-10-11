9324407a7fdbea0dc3d54713052ced46f2b09667
Romain <RomainFrechic@outlook.fr>
2016-10-11 16:35:30 +0200

    todo list style

--- app.js
+++ app.js
@@ -12,21 +12,10 @@ $(document).ready(function() {
 
  $('ul').on('click','li', function(){
 	$(this).toggleClass('cocher');
-		
+
 });
 
 
-
-	
-	$('.btn1').on('click','cocher',function(){
-           
-			var toAdd = $('input').val();
-
-		$('.done').prepend('<div>'+ toAdd +'</div>');
-		console.log();
-
-
-	});
 	
      
 
