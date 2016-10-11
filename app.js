67cac8e4a3596e5f6fd1df3867eb42f242e9cd09
Romain <RomainFrechic@outlook.fr>
2016-10-11 16:04:36 +0200

    to do list

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 
- $(document).ready(function() {
-    $('#button').click(function() {
-        var toAdd = $('input').val();
-        $('ul').append('<li>' + toAdd + '</li>')
-
-       
-        });
-    });
+$(document).ready(function() {
+	$('#button').click(function() {
+		var toAdd = $('input').val();
+		var t = $('ul').append('<li class="style2">' + toAdd + '</li>');
+		console.log(t);
 
 
-	$('ul').on('click','li', function(){
-	$(this).toggleClass('cocher');
-	
+	});
 });
+
+
+ $('ul').on('click','li', function(){
+	$(this).toggleClass('cocher');
+		
+});
+
+
+
+	
+	$('.btn1').on('click','cocher',function(){
+           
+			var toAdd = $('input').val();
+
+		$('.done').prepend('<div>'+ toAdd +'</div>');
+		console.log();
+
+
+	});
+	
+     
+
