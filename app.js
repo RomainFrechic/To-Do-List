8d4a49b5fa4024d45a11dcf9c7b2d876124aaec5
Romain <RomainFrechic@outlook.fr>
2016-10-11 13:35:52 +0200

    to do list in progress two

--- app.js
+++ app.js
@@ -1,32 +1,15 @@
 
  $(document).ready(function() {
     $('#button').click(function() {
-        var toAdd = $('input[name=checkListItem]').val();
-       var t=  $('.list').append('<div class="item"><input type="checkbox" name="cocher"><span>' + toAdd + '</span></input></div>');
-          console.log(toAdd);
-          console.log(t);
-        $('.item').on('click', function() {
-          // $(this).remove();
-          $(this).text('EFFECTUER '+ toAdd);
+        var toAdd = $('input').val();
+        $('ul').append('<li>' + toAdd + '</li>')
+
+       
         });
     });
+
+
+	$('ul').on('click','li', function(){
+	$(this).toggleClass('cocher');
+	
 });
-
-/*$('input.checkbox').click(function(){
- ('div.list').text('text-decoration','line-through');
-
-})
-
-var trait;
-$('div.list');
-$('trait').on('click',function(){
-
-	//if( $('input[name=cocher]').is(':checked') ){
- //$("div.list").classeList.toggle('checked');
-//} else {
-   // alert("jQuery c'est autre chose");
-   trait.
-}//
-
-});*/
-
